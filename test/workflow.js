6df56625c837cb4387fc323ceecd42673ca89e8c
Lee Dohm <lee@lee-dohm.com>
2017-02-12 06:21:43 -0800

    Pass context to Workflow.matches

--- test/workflow.js
+++ test/workflow.js
@@ -5,37 +5,37 @@ describe('Workflow', () => {
   describe('matches', () => {
     it('is truthy for matching event', () => {
       const workflow = new Workflow(['issues']);
-      expect(workflow.matches({event: 'issues', payload: {}})).toBeTruthy();
+      expect(workflow.matches({event: {event: 'issues', payload: {}}})).toBeTruthy();
     });
 
     it('is truthy for multiple events', () => {
       const workflow = new Workflow(['issues', 'pull_request']);
-      expect(workflow.matches({event: 'pull_request', payload: {}})).toBeTruthy();
+      expect(workflow.matches({event: {event: 'pull_request', payload: {}}})).toBeTruthy();
     });
 
     it('is truthy for event with action', () => {
       const workflow = new Workflow(['issues.opened']);
       expect(
-        workflow.matches({event: 'issues', payload: {action: 'opened'}})
+        workflow.matches({event: {event: 'issues', payload: {action: 'opened'}}})
       ).toBeTruthy();
     });
 
     it('is truthy for multiple events with action', () => {
       const workflow = new Workflow(['issues.opened', 'issues.labeled']);
       expect(
-        workflow.matches({event: 'issues', payload: {action: 'labeled'}})
+        workflow.matches({event: {event: 'issues', payload: {action: 'labeled'}}})
       ).toBeTruthy();
     });
 
     it('is falsy for different event', () => {
       const workflow = new Workflow(['issues']);
-      expect(workflow.matches({event: 'pull_request', payload: {}})).toBeFalsy();
+      expect(workflow.matches({event: {event: 'pull_request', payload: {}}})).toBeFalsy();
     });
 
     it('is falsy for different action', () => {
       const workflow = new Workflow(['issues.opened']);
       expect(
-        workflow.matches({event: 'issues', payload: {action: 'labeled'}})
+        workflow.matches({event: {event: 'issues', payload: {action: 'labeled'}}})
       ).toBeFalsy();
     });
   });
